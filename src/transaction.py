3e33738f7a35064409d7576f960fe9e7557774ce
Sabrout <omarsabrout@gmail.com>
2018-02-07 19:11:27 +0100

    test: setting up the first test for transaction
    adding an example for the transaction class in the main method to verify
    implementation

--- src/transaction.py
+++ src/transaction.py
@@ -18,6 +18,7 @@ class Transaction:
         # Checking format of list_input
         for i in list_input:
             (hash, output) = i
+            print(hash)
             if sys.getsizeof(hash) != 32:
                 raise Exception('INVALID HASH SIZE ERROR')
             if sys.getsizeof(output) != 1:
@@ -45,6 +46,39 @@ class Transaction:
 
         self.hash.update(self.hashable_string)
         self.hash.digest()
+
+
+def main():
+    # Generating list_input
+    list_input = list()
+    list_input.append(('063E418310654DACA9B72F05F5FDF8E2', 0))
+    list_input.append(('8D6D35CC70BA14FBE1DBCA3057791135', 1))
+    list_input.append(('5EFC60B80FA21BAA18D66C9A9C33C51E', 1))
+    # Generating list_wallet
+    list_wallet = list()
+    list_wallet.append('063E418310654DACA9B72F05F5FDF8E2E')
+    list_wallet.append('8D6D35CC70BA14FBE1DBCA3057791135E')
+    list_wallet.append('5EFC60B80FA21BAA18D66C9A9C33C51EE')
+    # Generating list_amount
+    list_amount = list()
+    list_amount.append('1000')
+    list_amount.append('2000')
+    list_amount.append('3000')
+    list_amount.append('4000')
+    # Creating the transaction
+    trans = Transaction(list_input, list_wallet, list_amount)
+    # Printing Statements
+    print(trans.list_input)
+    print(trans.list_wallet)
+    print(trans.output_number0)
+    print(trans.output_number1)
+    print(trans.list_amount)
+    print(trans.hashable_string)
+    print(trans.hash)
+
+
+if __name__ == "__main__":
+    main()
 
 
 
