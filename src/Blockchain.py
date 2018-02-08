f37ef0e945805cb6369957d53e0c219b5f0ec8b6
KESKIN Mustafa <mouss42490@gmail.com>
2018-02-08 16:33:00 +0100

    feat: initial commit

---
+++ src/Blockchain.py
@@ -0,0 +1,40 @@
+import hashlib
+
+class Stack:
+    def __init__(self):
+        blockchain = []
+
+    def isEmpty(self):
+        return blockchain == []
+
+    def push(self, item):
+        blockchain.append(item)
+
+    def pop(self):
+        blockchain.items.pop()
+
+    def peek(self):
+        return blockchain[len(blockchain) - 1]
+
+    def size(self):
+        return len(blockchain)
+
+
+class Cheese:
+    def __init__(self, smell, parentsmell, nonce, data):
+        self.smell=smell
+        self.parentsmell=parentsmell
+        self.nonce=nonce
+        self.data=data
+
+    def mining_cheese(self):
+        m=hashlib.sha256()
+        m.update(b(str(nonce)+b(str(cheese.data))))
+        if (m.display()==(number_of_zeros)+(cheese.data):
+
+
+
+    def verifyCheese:
+
+
+
