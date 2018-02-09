6ee23d893219791ac919f1338a51382d598cdf57
KESKIN Mustafa <mouss42490@gmail.com>
2018-02-09 18:14:51 +0100

    Feat: add functions for setting and accessing to cheese in the blockchain

--- src/Blockchain.py
+++ src/Blockchain.py
@@ -2,24 +2,41 @@ import hashlib
 import random
 
 
-class Stack:
+class CheeseStack:
     def __init__(self):
         self.blockchain = []
+        self.index = {}
 
     def isEmpty(self):
         return self.blockchain == []
 
-    def push(self, item):
-        self.blockchain.append(item)
+    def push(self, cheese):
+        self.blockchain.append(cheese)
+        self.index[cheese.smell]=len(self.blockchain)-1
 
-    def pop(self):
-        self.blockchain.items.pop()
-
-    def peek(self):
+    def last(self):
         return self.blockchain[len(self.blockchain) - 1]
 
     def size(self):
         return len(self.blockchain)
+
+    def __getitem__(self, parent_smell):
+        i=self.index[parent_smell]+1
+        try:
+            return self.blockchain[i]
+        except(IndexError):
+            return None
+
+    def __setitem__(self, parent_smell, cheese):
+        if not(isinstance(cheese, Cheese)):
+            raise Exception("Error: not a cheese")
+
+        if self.last().smell != parent_smell:
+            raise Exception("Error: bad parent smell")
+
+        push(cheese) 
+
+
 
 
 class Cheese:
@@ -43,7 +60,7 @@ class Cheese:
         return self.mine()
 
     def compute_smell(self):
-        hash = hashlib.sha256()
+        smell = hashlib.sha256()
         string = str(self.parentsmell).encode() + str(self.data).encode() + str(self.nonce).encode()
 
         hash.update(string)
@@ -54,8 +71,3 @@ class Cheese:
         if self.verify_policy():
             return True
         return False
-
-
-
-
-
