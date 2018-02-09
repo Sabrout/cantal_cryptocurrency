5d1b3ff95ee5ab5a9c9edf980e7a308ad25cc968
KESKIN Mustafa <mouss42490@gmail.com>
2018-02-09 17:12:00 +0100

    feat:add functions to verify cheese

--- src/Blockchain.py
+++ src/Blockchain.py
@@ -1,23 +1,25 @@
 import hashlib
+import random
+
 
 class Stack:
     def __init__(self):
-        blockchain = []
+        self.blockchain = []
 
     def isEmpty(self):
-        return blockchain == []
+        return self.blockchain == []
 
     def push(self, item):
-        blockchain.append(item)
+        self.blockchain.append(item)
 
     def pop(self):
-        blockchain.items.pop()
+        self.blockchain.items.pop()
 
     def peek(self):
-        return blockchain[len(blockchain) - 1]
+        return self.blockchain[len(self.blockchain) - 1]
 
     def size(self):
-        return len(blockchain)
+        return len(self.blockchain)
 
 
 class Cheese:
@@ -27,14 +29,33 @@ class Cheese:
         self.nonce=nonce
         self.data=data
 
-    def mining_cheese(self):
-        m=hashlib.sha256()
-        m.update(b(str(nonce)+b(str(cheese.data))))
-        if (m.display()==(number_of_zeros)+(cheese.data):
+    def verify_policy(self, num_zero=1):
+        for i in range(num_zero):
+            if (self.smell[i]!="0"):
+                return False
+        return True
+
+    def mine(self):
+        self.nonce=int(str(random.random()).replace(",",""))
+        self.compute_smell()
+        if  self.verify_policy():
+            return True
+        return self.mine()
+
+    def compute_smell(self):
+        hash = hashlib.sha256()
+        string = str(self.parentsmell).encode() + str(self.data).encode() + str(self.nonce).encode()
+
+        hash.update(string)
+        self.smell = hash.display()
+
+    def verify(self):
+        self.compute_smell()
+        if self.verify_policy():
+            return True
+        return False
 
 
-
-    def verifyCheese:
 
 
 
