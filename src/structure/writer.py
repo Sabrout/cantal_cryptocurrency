90e86c46101d7a544b90c472412b724c36bfa751
Paul Viallard <paul.viallard@gmail.com>
2018-02-10 16:53:14 +0100

    feat: add the writer of the cheese stack

---
+++ src/structure/writer.py
@@ -0,0 +1,101 @@
+from src.structure.cheese import CheeseStack
+from src.structure.cheese import Cheese
+
+
+class CheeseStackWriter():
+    """
+    The class CheeseStackWriter will take the cheese stack
+    and it will interpret the object to save it
+    """
+    def __init__(self, stack, path):
+        """
+        We constructor will save the stack
+        """
+        self.file = open(path, "wb")
+        self.stack = stack
+
+    def write(self):
+        """
+        Write the stack in the file
+        """
+        self.write_stack()
+
+    def write_stack(self):
+        """
+        We write all the stack in a file
+        """
+        for i in range(self.size_file, len(self.stack)-1):
+            self.write_cheese(self.stack[i])
+            self.file.write(",")
+        self.write_cheese(self.stack[len(self.stack)-1])
+
+    def write_cheese(self, cheese):
+        """
+        Write a cheese in a file
+        """
+        self.file.write(cheese.hash+";")
+        self.write_trasaction_list(cheese.data)
+        self.file.write(";"+str(cheese.nonce))
+
+    def write_transaction_list(self, transaction_list):
+        """
+        Write the transaction list
+        """
+        for i in range(0, len(transaction_list)-1):
+            self.write_transaction(transaction_list[i])
+            self.file.write(",")
+        self.write_transaction(transaction_list[len(transaction_list)-1])
+
+    def write_transaction(self, transaction):
+        """
+        Write a transaction
+        """
+        self.write_input_list(transaction)
+        self.file.write(",")
+        self.write_wallet_list(transaction)
+        self.file.write(",")
+        self.write_amount_list(transaction)
+        self.file.write(",")
+        self.write_sign_list(transaction)
+
+    def write_input_list(self, transaction):
+        """
+        Write the list of inputs
+        """
+        for i in range(0, len(transaction.list_input)-1):
+            (hash, output) = transaction.list_input[i]
+            self.file.write(hash+",")
+            self.file.write(str(output)+",")
+        (hash, output) = transaction.list_input[len(transaction.list_input)-1]
+        self.file.write(hash+",")
+        self.file.write(str(output))
+
+    def write_wallet_list(self, transaction):
+        """
+        Write the list of wallets
+        """
+        for i in range(0, len(transaction.list_wallet)-1):
+            wallet = transaction.list_wallet[i]
+            self.file.write(wallet+"-")
+        wallet = transaction.list_wallet[len(transaction.list_wallet)-1]
+        self.file.write(wallet)
+
+    def write_amount_list(self, transaction):
+        """
+        Write the list of amounts
+        """
+        for i in range(0, len(transaction.list_amount)-1):
+            amount = transaction.list_amount[i]
+            self.file.write(str(amount)+"-")
+        amount = transaction.list_amount[len(transaction.list_amount)-1]
+        self.file.write(str(amount))
+
+    def write_sign_list(self, transaction):
+        """
+        Write the list of signatures
+        """
+        for i in range(0, len(transaction.list_sign)-1):
+            sign = transaction.list_sign[i]
+            self.file.write(sign+"-")
+        sign = transaction.list_sign[len(transaction.list_sign)-1]
+        self.file.write(sign)
