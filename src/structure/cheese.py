b9353cc2264bb7ee9c6d9f710a8abd8989c5d2e9
KESKIN Mustafa <mouss42490@gmail.com>
2018-02-11 17:08:35 +0100

    feat: some corrections and start comments for cheese and cheese_stack

--- src/structure/cheese.py
+++ src/structure/cheese.py
@@ -3,6 +3,9 @@ import random
 
 
 class Cheese:
+    """
+    Cheese is the component of the blockchain
+    """
     def __init__(self, smell=None, parent_smell=None, nonce=None, data=None):
         self.smell = smell
         self.parent_smell = parent_smell
@@ -10,24 +13,39 @@ class Cheese:
         self.data = data
 
     def set_smell(self, smell):
+        """
+        The smell of a cheese is the hash of the parent smell + data + nonce
+        """
         self.smell = smell
 
     def set_parent_smell(self, parent_smell):
+        """
+        Every cheese has to contain the smell of the previous cheese
+        """
         self.parent_smell = parent_smell
 
     def set_nonce(self, nonce):
         self.nonce = nonce
 
     def set_data(self, data):
+        """
+        Set the data of a cheese
+        """
         self.data = data
 
     def verify_policy(self, num_zero=1):
+        """
+        Verify that the smell of a cheese have the right number of zeros
+        """
         for i in range(num_zero):
             if (self.smell[i] != "0"):
                 return False
         return True
 
     def mine(self):
+        """
+        Mining a cheese by generating random nonces
+        """
         self.nonce = int(str(random.random()).replace(",", ""))
         self.compute_smell()
         if self.verify_policy():
@@ -35,6 +53,9 @@ class Cheese:
         return self.mine()
 
     def compute_smell(self):
+        """
+        Compute the smell of a cheese
+        """
         hash = hashlib.sha256()
         string = str(self.parent_smell).encode()
         + str(self.data).encode()
@@ -44,6 +65,10 @@ class Cheese:
         self.smell = hash.display()
 
     def verify(self):
+        """
+        Verify that a cheese is true (right number of zeros in the smell and
+        that world appears only once in transactions
+        """
         self.compute_smell()
         if not(self.verify_policy()):
             return False
--- src/structure/cheese_stack.py
+++ src/structure/cheese_stack.py
@@ -2,31 +2,56 @@ from src.structure.cheese import Cheese
 
 
 class CheeseStack:
+    """
+    This is the blockchain
+    """
     def __init__(self):
+        """
+        The blockchain is a list of cheeses and each cheese have an index
+        that corresponds to his index in the blockchain (the key is the smell)
+        """
         self.size_file = 0
         self.blockchain = []
         self.block_index = {}
         self.transaction_index = {}
 
     def isEmpty(self):
+        """
+        Return true if the blockchain is empty
+        """
         return self.blockchain == []
 
+    #To do verif
     def push(self, cheese):
+        """
+        Set a cheese in the blockchain
+        """
         self.blockchain.append(cheese)
         self.block_index[cheese.smell] = len(self.blockchain)-1
 
+        #Each cheese contains a list of transactions
+        #Here we set the value of the index of the cheese for the key hash of each transactions
         for transaction in cheese.data:
             self.transaction_index[transaction.hash] = len(self.blockchain)-1
 
+            #
             for (hash, output) in transaction.list_input:
                 input_block = self.transaction_index[hash]
                 input_transaction = self.blockchain[input_block].data.get(hash)
                 input_transaction.used_output[output] = transaction.hash
 
     def last(self):
+        """
+        :return: The last cheese
+        """
         return self.blockchain[len(self.blockchain) - 1]
 
     def verify(self, cheese):
+        """
+        Verify that a cheese is able to be push in the blockchain
+        (it have to be a cheese, have the parent smell, and the
+        right number of zeros in his smell, and finally a correct transactions set).
+        """
         if not(isinstance(cheese, Cheese)):
             raise Exception("Error: not a cheese")
 
@@ -43,9 +68,9 @@ class CheeseStack:
                 input_block = self.transaction_index[hash]
                 input_transaction = self.blockchain[input_block].data.get(hash)
 
-                wallet = transaction.list_wallet[-output+1]
+                wallet = transaction.list_wallet[output-2]
                 if output == 0:
-                    money = transaction.list_amount[-output+1]
+                    money = transaction.list_amount[-1]
                 else:
                     money = sum(transaction.list_amount[:-1])
                     money -= transaction.list_amount[-1]
