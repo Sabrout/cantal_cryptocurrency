ebb813d2446adcd875fd6256b9cbb63937ea69cd
Sabrout <omarsabrout@gmail.com>
2018-02-14 13:16:16 +0100

    fix: fixed a typo and added comments :P

--- src/tracker/tracker.py
+++ src/tracker/tracker.py
@@ -1,4 +1,17 @@
 from src.tracker.member_list import MemberList
+from src.network.peer import Peer
 
-# class Tracker:
 
+class Tracker(Peer):
+    def __init__(self, IP, port):
+        Peer.__init__(self, IP, port)
+        self.list = MemberList()
+
+    def add_member(self, new_member):
+        self.list.add_member(new_member)
+
+    def remove_member(self, member):
+        self.list.remove_member(member)
+
+    def get_sublist(self):
+        return self.get_sublist()
