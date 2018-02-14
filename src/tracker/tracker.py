d4eba16ff5447332a35437feebd39da00ff174c6
Sabrout <omarsabrout@gmail.com>
2018-02-14 14:25:02 +0100

    feat: Initial tracker.py implementation completed

--- src/tracker/tracker.py
+++ src/tracker/tracker.py
@@ -11,19 +11,34 @@ class Tracker(Peer):
     def process_message(self, tuple):
         (socket, message) = tuple
         ip = socket.getsockname()[0]
-        # Handling Requests
-        if message.packet_type == Message.REQUEST:
+        # Handling Messages
+
+        if message.get_packet() == Message.LIST:
+
             # List REQUEST
-            if message.packet == Message.LIST:
-                # Creating a RESPONSE message
-                response = Message()
-                response.set_packet(Message.LIST)
-                response.set_packet_type(Message.RESPONSE)
-                response.set_data(self.list.get_sublist())
+            # Creating a RESPONSE message
+            response = Message()
+            response.set_packet(Message.LIST)
+            response.set_packet_type(Message.RESPONSE)
+            response.set_data(self.list.get_sublist())
+            if message.get_packet_type() == Message.REQUEST:
                 # Adding member to list
+                try:
+                    port = int(message.get_data())
+                except ValueError:
+                    raise Exception('Error: Invalid Port')
+                self.list.add_member((ip, port))
+                Peer.produce_response(self, socket, response)
 
-                self.list.add_member((ip, int(message.data)))
+            # List ERROR
+            if message.get_packet_type() == Message.ERROR:
+                Peer.produce_response(self, socket, response)
 
-
-
-print(int('a11'))
+        # Member REPORT
+        if message.get_packet_type() == Message.REPORT and message.get_packet() == Message.MEMBER:
+            # Removing member
+            try:
+                port = int(message.get_data())
+            except ValueError:
+                raise Exception('Error: Invalid Port')
+            self.list.remove_member((ip, port))
