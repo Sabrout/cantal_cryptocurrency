0aac936b3072792ae17579eb40dbd5f76ae5e518
Sabrout <omarsabrout@gmail.com>
2018-03-03 16:37:21 +0100

    test: initiated money_list test

---
+++ tests/member/money_list.py
@@ -0,0 +1,21 @@
+import unittest
+from src.member.money_list import MoneyList
+
+
+class MoneyListTest(unittest.TestCase):
+
+    def test_add_member_pos(self):
+        list = MoneyList()
+        list.add_money(('172.0.0.1', '8080'))
+        self.assertEqual(list.is_member(('172.0.0.1', '8080')), True)
+
+    def test_add_member_neg(self):
+        list = MoneyList()
+        self.assertEqual(list.is_member(('172.0.0.1', '8080')), False)
+
+    def test_remove_member_pos(self):
+        list = MoneyList()
+        list.add_member(('172.0.0.1', '8080'))
+        # Removing from an existing list
+        list.remove_member(('172.0.0.1', '8080'))
+        self.assertEqual(list.is_member(('172.0.0.1', '8080')), False)
