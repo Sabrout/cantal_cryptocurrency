b6bd3df793b621f46b128791a24d076850116c4d
Paul Viallard <paul.viallard@gmail.com>
2018-03-03 12:47:57 +0100

    feat: add the functions for the member

--- src/member/member.py
+++ src/member/member.py
@@ -3,19 +3,26 @@ from src.network.peer import Peer
 class Member(Peer):
     def __init__(self, port):
         Peer.__init__(self, port)
-        self.member_list = None
+        self.member_list = MemberList()
+        self.cheese_stack = CheeseStack()
+        self.member_list = Ressource(self.member_list)
+        self.cheese_stack = Ressource(self.cheese_stack)
+
         self.transaction_list = None
         self.monney_list = None
-        self.blockchain = None
 
-    def set_member_list(self, member_l):
-        self.member_list = member_l
+    def process_message(self):
+        return "Don't care"
 
-    def set_transaction_list(self, transaction_l):
-        self.transaction_list = transaction_l
+    def process_member_list(self):
+        def handle_thread():
+            return "Don't care"
+        t = Thread(target=handle_thread)
+        return t
 
-    def set_monney_list(self, monney_l):
-        self.monney_list = monney_l
+    def process_cheese_stack(self):
+        def handle_thread():
+            return "Don't care"
+        t = Thread(target=handle_thread)
+        return t
 
-    def set_blockchain(self, blockchain):
-        self.blockchain = blockchain
