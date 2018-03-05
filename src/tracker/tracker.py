1ff6d5516ed2fd99c9424a13367e5941d903494d
guillaumevidot <eric.guillaume.vidot@hotmail.fr>
2018-03-05 19:13:02 +0100

    feat: add ping/pong for testing the status of a member
    by "status" I mean if he is there or not

--- src/tracker/tracker.py
+++ src/tracker/tracker.py
@@ -8,8 +8,11 @@ import random
 class Tracker(Peer):
     def __init__(self, port):
         Peer.__init__(self, port)
-        self.list = MemberList()
+        self.member_list = MemberList()
+        self.member_list = Ressource(self.member_list)
         self.main().start()
+        self.process_member_list_ping(5).start()
+        self.process_member_list_pong().start()
 
     def process_message(self, tuple):
         (ip, socket, message) = tuple
@@ -22,12 +25,16 @@ class Tracker(Peer):
                 # Adding member to list
                 port = int(message.get_data())
 
-                self.list.add_member((ip, port))
+                member_list = self.member_list.ressource
+                self.member_list.write(member_list.add_member, (ip, port))
 
                 response = Message()
                 response.set_packet(Message.LIST)
                 response.set_packet_type(Message.RESPONSE)
-                response.set_data(self.list.get_sublist())
+
+                member_list = self.member_list.ressource
+                sublist = self.member_list.read(member_list.get_sublist)
+                response.set_data(sublist)
                 self.produce_response(socket=socket, message=response, close=True)
 
             # List ERROR
@@ -45,8 +52,9 @@ class Tracker(Peer):
             except ValueError:
                 raise Exception('Error: Invalid IP Port')
 
-            self.list.remove_member(ip_port)
-            print(self.list.print_list())
+            member_list = self.member_list.ressource
+            self.member_list.write(member_list.remove_member, ip_port)
+            print(self.member_list.read(member_list.print_list))
         else:
             print('Error: No Message Type Detected\n')
 
@@ -72,6 +80,34 @@ class Tracker(Peer):
             port = random.randint(1, 9999)
             list.add_member((ip, port))
         return list
+
+    def process_member_list_ping(self, sleep):
+        def handle_thread():
+            member_list = self.member_list.ressource
+            member = self.member_list.read(member_list.get_random)
+
+            if member is not None:
+                (ip, port) = member
+                self.produce_ping(ip, port)
+
+            time.sleep(sleep)
+
+            handle_thread()
+
+        t = Thread(target = handle_thread)
+        return t
+
+    def process_member_list_pong(self):
+        def handle_thread():
+            ip, port, pong = self.consume_pong()
+            if(not(pong)):
+                member_list = self.member_list.ressource
+                self.member_list.write(member_list.remove_list, (ip, port))
+
+            handle_thread()
+
+        t = Thread(target = handle_thread)
+        return t
 
 
 if __name__ == "__main__":
